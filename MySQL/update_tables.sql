be841b2c7f1657c6293adcffd707a921761cd7f4
meglee67 <123908362+meglee67@users.noreply.github.com>
2023-10-09 22:51:46 -0400

    Create update_tables.sql

---
+++ MySQL/update_tables.sql
@@ -0,0 +1,11 @@
+UPDATE contact_info
+SET email = 'astarion.ancunin@example.com'
+WHERE employee_id = 1;
+
+UPDATE contact_info
+SET email = 'gale.dekarios@example.com'
+WHERE employee_id = 2;
+
+UPDATE contact_info
+SET email = 'wyll.ravengard@example.com'
+WHERE employee_id = 3;
