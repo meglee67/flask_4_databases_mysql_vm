a95b24dedf42cd2eb571b412b3a04270dde23df7
meglee67 <123908362+meglee67@users.noreply.github.com>
2023-10-09 22:34:57 -0400

    Create create_tables.sql

---
+++ MySQL/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE employment;
+
+USE employment;
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    SSN VARCHAR(9) UNIQUE NOT NULL
+);
+
+CREATE TABLE contact_info (
+    contact_id INT PRIMARY KEY AUTO_INCREMENT,
+    employee_id INT UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    address TEXT,
+    phone_number VARCHAR(15),
+    email VARCHAR(100),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
